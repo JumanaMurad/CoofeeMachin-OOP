6a2f84129b24fea88e104fd81579f48a254d2aff
JumanaMurad <jumanamurad1@gmail.com>
2022-07-15 19:27:17 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+
+while is_on:
+    order = input(f"What would you like? ({menu.get_items()}): ").lower()
+
+    if order == 'off':
+        is_on = False
+    elif order == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
